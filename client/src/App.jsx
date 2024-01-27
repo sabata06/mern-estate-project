906d3115e79481620ed9ae01c69c3ff2ef6d29bb
Your Name <m.karaarslan06@outlook.com>
2024-01-27 21:10:05 +0300

    test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 
 const App = () => {
-  return <div>App</div>;
+  return <BrowserRouter>App</BrowserRouter>;
 };
 
 export default App;
