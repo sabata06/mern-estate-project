0abeeb80a6a33dac5d19e502d81d81ddb0484c35
Your Name <m.karaarslan06@outlook.com>
2024-03-04 23:30:12 +0300

    create listing page

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -4,8 +4,8 @@ const CreateListing = () => {
       <h1 className="text-3xl font-semibold text-center my-7">
         Create a Listing
       </h1>
-      <form className="flex flex-col sm:flex-row">
-        <div className="flex flex-col gap-4 flex-1">
+      <form className="flex flex-col sm:flex-row gap-4">
+        <div className="flex flex-col gap-4 flex-1 ">
           <input
             type="text"
             placeholder="Name"
@@ -102,6 +102,27 @@ const CreateListing = () => {
                 <span className="text-xs">($ / Month)</span>
               </div>
             </div>
+          </div>
+        </div>
+        <div className="flex flex-col flex-1 gap-4">
+          <p className="font-semibold">
+            Images:
+            <span className="font-normal text-gray-600 ml-2 ">
+              The first image will be the cover (Max 6)
+            </span>
+          </p>
+          <div className="flex gap-4">
+            <input
+              className="p-3 border border-gray-300 rounded w-full"
+              type="file"
+              id="images"
+              accept="image/*"
+              multiple
+              required
+            />
+            <button className="p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80 ">
+              Upload
+            </button>
           </div>
         </div>
       </form>
