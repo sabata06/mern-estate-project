95ee1b4fd7b0af4f660d43fc7db529386542287e
Your Name <m.karaarslan06@outlook.com>
2024-03-14 01:02:28 +0300

    complate show user listings functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -29,6 +29,7 @@ const Profile = () => {
   const dispatch = useDispatch();
   const [updateSuccess, setUpdateSuccess] = useState();
   const [showListingsError, setShowListingsError] = useState(false);
+  const [userListings, setUserListings] = useState([]);
 
   // fireabase storage
   // allow read;
@@ -133,6 +134,7 @@ const Profile = () => {
         setShowListingsError(true);
         return;
       }
+      setUserListings(data);
     } catch (error) {
       setShowListingsError(true);
     }
@@ -225,6 +227,37 @@ const Profile = () => {
       <p className="text-red-700 mt-5">
         {showListingsError && "Error showing listings..."}
       </p>
+      {userListings && userListings.length > 0 && (
+        <div className="flex flex-col gap-4">
+          <h1 className="text-2xl font-semibold text-center mt-7">
+            Your Listings
+          </h1>
+          {userListings.map((listing) => (
+            <div
+              key={listing._id}
+              className="border rounded-lg p-3 flex justify-between items-center gap-4"
+            >
+              <Link to={`/listing/${listing._id}`}>
+                <img
+                  src={listing.imageUrls[0]}
+                  alt="listing cover"
+                  className="h-16 w-16 object-contain"
+                />
+              </Link>
+              <Link
+                className="text-slate-700 font-semibold flex-1 hover:underline truncate"
+                to={`/listing/${listing._id}`}
+              >
+                <p>{listing.name}</p>
+              </Link>
+              <div className=" flex flex-col items-center">
+                <button className="text-red-700 uppercase">delete</button>
+                <button className="text-green-700 uppercase">edit</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
